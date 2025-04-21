8170b4d71db2ef21a1b343feb8d2dafa656953ab
Waldey <waldeypereira@gmail.com>
2025-04-21 22:26:30 +0200

    controller created

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Controller.java
@@ -0,0 +1,19 @@
+package dev.java10x.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+
+public class Controller {
+
+    // Pegar informacoes
+    @GetMapping("/boasvindas")
+    public String boasVindas() {
+        return "Essa é minha primeira mensagem nessa rota!";
+    }
+
+}

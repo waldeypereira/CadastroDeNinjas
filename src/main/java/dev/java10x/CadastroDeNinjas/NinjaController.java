c01cfa52aeeee84d24ccc8863c47d64d3a2ecfbc
Waldey <waldeypereira@gmail.com>
2025-04-21 23:05:50 +0200

    entity class created in database from NinjaModel

--- src/main/java/dev/java10x/CadastroDeNinjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/NinjaController.java
@@ -1,14 +1,13 @@
 package dev.java10x.CadastroDeNinjas;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
 
-public class Controller {
+public class NinjaController {
 
     // Pegar informacoes
     @GetMapping("/boasvindas")
---
+++ src/main/java/dev/java10x/CadastroDeNinjas/NinjaModel.java
@@ -0,0 +1,53 @@
+package dev.java10x.CadastroDeNinjas;
+
+import jakarta.persistence.*;
+
+// Transforma uma classe em uma entidade do BD
+// Java Persistence API - JPA
+@Entity
+@Table(name = "tb_cadastro")
+public class NinjaModel {
+
+    @Id
+    // Numeros sequenciais
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String nome;
+    String email;
+    int idade;
+
+    // No args constructor
+    public NinjaModel() {
+    }
+
+    // All args constructor
+    public NinjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}

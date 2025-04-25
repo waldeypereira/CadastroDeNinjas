282b8d063170e6085ed06c46db271755b5c67590
Waldey <waldeypereira@gmail.com>
2025-04-25 11:05:06 +0200

    feat: Criado MissoesRepository Interface com JPA

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesRepository.java
@@ -0,0 +1,9 @@
+package dev.java10x.CadastroDeNinjas.Missoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissoesRepository extends JpaRepository<MissoesModel, Long> {
+
+
+
+}
